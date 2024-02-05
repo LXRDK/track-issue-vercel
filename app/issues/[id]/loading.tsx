09ec86e49b0aebcda6c3cd59650ab00003977bcb
Lxrdk <83618907+LXRDK@users.noreply.github.com>
2024-02-06 02:07:51 +0300

    show issue details

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoadingIssueDetailPage = () => {
+  return <div></div>;
+};
+
+export default LoadingIssueDetailPage;
---
+++ app/issues/new/loading.tsx
@@ -0,0 +1,12 @@
+import { Skeleton } from "@radix-ui/themes";
+import React from "react";
+
+const LoadingNewIssuelPage = () => {
+  return (
+    <div>
+      <Skeleton className="min-h-screen" />
+    </div>
+  );
+};
+
+export default LoadingNewIssuelPage;
