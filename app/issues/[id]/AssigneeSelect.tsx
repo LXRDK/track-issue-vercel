b0063a0a02efcc9ec5393dd31989b5d4f43d4714
Lxrdk <83618907+LXRDK@users.noreply.github.com>
2024-02-11 14:23:14 +0300

    refactor

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -6,18 +6,25 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get("/api/users").then((res) => res.data),
-    staleTime: 60 * 1000,
-    retry: 3,
-  });
+  const { data: users, error, isLoading } = useUsers();
   if (isLoading) return <Skeleton width={"10rem"} />;
   if (error) return null;
+  const assignIssue = async (userId: string) => {
+    try {
+      await axios.patch("/api/issues/" + issue.id, {
+        assignedToUserId: userId !== "Unassigned" ? userId : null,
+      });
+      try {
+        const user = await users?.filter((user) => user.id == userId);
+        toast.success("assigned to " + user![0].name);
+      } catch (err) {
+        toast.success("unassigned");
+      }
+    } catch (err) {
+      console.log(err);
+      toast.error("Changes cant be made");
+    }
+  };
   return (
     <>
       <Toaster />
@@ -25,22 +32,7 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
         defaultValue={
           issue.assignedToUserId ? issue.assignedToUserId : "Unassigned"
         }
-        onValueChange={async (userId) => {
-          try {
-            await axios.patch("/api/issues/" + issue.id, {
-              assignedToUserId: userId !== "Unassigned" ? userId : null,
-            });
-            try {
-              const user = await users?.filter((user) => user.id == userId);
-              toast.success("assigned to " + user![0].name);
-            } catch (err) {
-              toast.success("unassigned");
-            }
-          } catch (err) {
-            console.log(err);
-            toast.error("Changes cant be made");
-          }
-        }}
+        onValueChange={assignIssue}
       >
         <Select.Trigger placeholder="Unassigned"></Select.Trigger>
         <Select.Content>
@@ -58,5 +50,11 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
-
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
 export default AssigneeSelect;
